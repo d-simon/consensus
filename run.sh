2d12bc3e1fd9b699281f67901e892be6379a1441
David Simon <me@davidsimon.ch>
2017-07-03 01:47:08 +0200

    [Update] Download DAG inside machine and only download DAG if it doesnt already exist

--- run.sh
+++ run.sh
@@ -1,6 +1,12 @@
 # Genesis
 geth init /app/genesis.json --datadir /data/ethData
 
+
+if [ -f "~/.ethash/full-R23-000000000000000" ]; then
+else
+  wget https://0xfff.io/dag/full-R23-0000000000000000 && mv full-R23-0000000000000000 ~/.ethash
+fi
+
 # Each worker needs a local account to store ETH.   Make sure you use /data so that when the container is rebuilt/redeployed, you won't lose your data
 echo "ethpasswd" > /data/ethpassword
 geth --datadir /data/ethData --password /data/ethpassword account new
